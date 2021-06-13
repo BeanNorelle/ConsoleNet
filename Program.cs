2c87e3b203a7fa4a85f2ca3b96eb069374fd670d
BeanSobre <beansobre@gmail.com>
2021-06-14 03:13:32 +0800

    second commit

--- Program.cs
+++ Program.cs
@@ -7,12 +7,34 @@ namespace Get_ipaddress
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
            // listAddress.loadAddress();
           
           string server = Dns.GetHostName();
-          Console.WriteLine("Hostname ............................ "+server);
+            Console.WriteLine("sdadssakjalwekwjlj");
+            string KeyRead = Console.ReadLine();
+           // Console.ReadKey();
+      
+        }
+
+          void ipaddress(string server){
+               foreach(var s in listAddress.loadAddress(server)){
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("ScopeID............{0}",s.scopeId,Console.ForegroundColor);
+                Console.WriteLine("Address............{0}",s.address);
+                Console.WriteLine("Address Family.....{0}",s.addressFamily);
+                Console.WriteLine("Address Bytes......{0}",s.getBytes);
+                Console.WriteLine("Hashtype...........{0}",s.getHashCode);
+                Console.WriteLine("Type...............{0}",s.getType);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(".............................................",Console.ForegroundColor);
+            } 
+        }
+
+         void getIpPrint(string server) 
+        {
+                 Console.WriteLine("Hostname ............................ "+server);
 
             foreach(NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
             {
@@ -33,25 +55,11 @@ namespace Get_ipaddress
                 string line="";  for(int n=nLength;n<=35;n++){
                  line+=".";  
                 }return line;}
-        
+            
              Console.ReadKey();
-          
         }
-
-        void ipaddress(string server){
-               foreach(var s in listAddress.loadAddress(server)){
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("ScopeID............{0}",s.scopeId,Console.ForegroundColor);
-                Console.WriteLine("Address............{0}",s.address);
-                Console.WriteLine("Address Family.....{0}",s.addressFamily);
-                Console.WriteLine("Address Bytes......{0}",s.getBytes);
-                Console.WriteLine("Hashtype...........{0}",s.getHashCode);
-                Console.WriteLine("Type...............{0}",s.getType);
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine(".............................................",Console.ForegroundColor);
-            }
-        }
+     }
 
 
     }
-}
+
